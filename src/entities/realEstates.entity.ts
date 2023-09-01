b661997fe4461094a06ca890c6910d2e39eae334
rcoelhomf <rcoelhomf@hotmail.com>
2023-09-01 16:22:04 -0300

    fix: Entity relation

--- src/entities/realEstates.entity.ts
+++ src/entities/realEstates.entity.ts
@@ -24,7 +24,7 @@ export class RealEstate {
     updatedAt: string;
 
     @OneToMany(() => Schedule, (schedules) => schedules.realEstate)
-    schedules: Schedule;
+    schedules: Schedule[];
 
     @OneToOne(() => Address, (adresses) => adresses.realEstate)
     @JoinColumn()
